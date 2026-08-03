7ef9dab6974f270e3724d87d605198739ea31a84
Hasssan3xl <elhassanehussaine@gmail.com>
2026-08-03 14:50:55 +0100

    added whitenoise and static root

--- src/settings/base.py
+++ src/settings/base.py
@@ -122,3 +122,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Use WhiteNoise's compressed manifest storage for production-ready static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
